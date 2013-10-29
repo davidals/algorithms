d158e38e2c67bae5640253ddbbdc4de06ae5c023
David Anderson <david.anderson.lino@gmail.com>
2013-10-29 20:48:49 -0300

    Digital Root

---
+++ csharp/VasyaDigitalRoot.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeForces
+{
+    class VasyaDigitalRoot
+    {
+         public static void Main()
+        {
+            //TextReader reader = new StreamReader("/test.txt");
+            Answer(Console.In);
+            Console.ReadLine();
+
+        }
+
+        public static void Answer(TextReader reader)
+         {
+             int[] array = reader.ReadLine().Split(' ').Select(x => Convert.ToInt32(x)).ToArray();
+             int k = array[0];
+             int d = array[1];
+
+            if(d == 0 && k != 1)
+                Console.WriteLine("No solution");
+
+            else
+            {
+                Console.Write(d);
+                for(int i = 1; i < k; i++)
+                    Console.Write(0);
+                Console.WriteLine();
+            }
+         }
+    }
+}
